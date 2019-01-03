ec433007b78e93620883c274aee1f05b60bf666a
ToluAkin <toluakintoye@gmail.com>
2019-01-03 02:05:40 +0100

    modal text added

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@ $(document).ready(function(){
   		$('#myModal').modal('show');
 	});
 });
-// var modal = document.querySelector(".modal");
-// var closeButton = document.querySelector(".close-button");
+var modal = document.querySelector(".modal");
+var closeButton = document.querySelector(".close-button");
 
-// function toggleModal() {
-//     modal.classList.toggle("show-modal");
-// }
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
 
-// function windowOnClick(event) {
-//     if (event.target === modal) {
-//         toggleModal();
-//     }
-// }
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
 
-// trigger.addEventListener("click", toggleModal);
-// closeButton.addEventListener("click", toggleModal);
-// window.addEventListener("click", windowOnClick);
+pop.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
